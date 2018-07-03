9fd1ba2edf2525bcffdf0ba89637ead2925a9396
Pawel Klasa <pawel.klasa@gmail.com>
2018-07-03 16:05:06 +0100

    init react setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'production'),
+    // below line only works for webpack 1.0
+    // path: './dist', 
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  }
+}
